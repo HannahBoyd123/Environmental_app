e06f075bcd080a503fa2ce61faf93fce4c1fdea4
Hannah B <hannahboyd345@gmail.com>
2022-06-12 20:31:46 +0100

    Update connect_to_db.py

--- Environment-Project/climate/website/connect_to_db.py
+++ Environment-Project/climate/website/connect_to_db.py
@@ -3,7 +3,7 @@ import mysql.connector
 def _connect_to_db(db_name: str):
     cnx = mysql.connector.connect(host='localhost',
                                   user='root',
-                                  password='&Potato98&',
+                                  password='******',
                                   auth_plugin='mysql_native_password',
                                   database=db_name)
     return cnx
